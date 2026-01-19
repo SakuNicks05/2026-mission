32ef05b8c4457abad1a1f0a8175acc6304773292
saku nicks <saku@gmail.com>
2026-01-20 07:19:18 +0800

    LOG IN

--- back-end/log_in.py
+++ back-end/log_in.py
@@ -3,7 +3,31 @@ import pandas as pd
 import sqlite3
 
 def log_in():
-    pass
+    connection = sqlite3.connect("../database/zion.db")
+    cursor = connection.cursor()
+
+    cursor.execute("SELECT * FROM users")
+    table = cursor.fetchall()
+
+    for row in table:
+        print(row)
+
+    username = input("Enter username: ")
+    password = input("Enter password: ")
+
+    match_credentials = """
+        SELECT password
+        FROM users
+        WHERE password = ?
+    """
+
+    cursor.execute(match_credentials, (password,))
+    pass_matched = cursor.fetchone()
+
+    if password == pass_matched[2]:
+        print("Logged in successfully!")
+    else:
+        print("Password didn't match. Log in failed.")
 
 def testing():
     connection = sqlite3.connect("../database/zion.db")
@@ -26,4 +50,4 @@ def testing():
         print(f"NAME: {user_match[1]}\n")
         print(f"PASSWORD: {user_match[2]}\n")
 
-testing()
+log_in()
